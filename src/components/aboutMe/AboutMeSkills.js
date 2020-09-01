b2c9e6b83c2ab3f99f36af892f831a5b9b2ee6fb
Alba Gómez Pérez <albagope@gmail.com>
2020-09-01 16:33:08 +0200

    make responsive about me section

--- src/components/aboutMe/AboutMeSkills.js
+++ src/components/aboutMe/AboutMeSkills.js
@@ -6,9 +6,9 @@ const AboutMeSkills = () => {
   return (
     <div className="generalStatus__container">
       <Row className="generalStatus__container">
-        <p className="results__title" >Mis habilidades </p>
+        <p className="results__title">Mis habilidades</p>
           <span className="skills-description">El equilibrio adecuado entre mis habilidades técnicas y personales es la clave para el desarrollo de mis proyectos</span>
-        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 17, offset: 4 }}>
+        <Col>
           <SkillsProgress />
         </Col>
       </Row>
