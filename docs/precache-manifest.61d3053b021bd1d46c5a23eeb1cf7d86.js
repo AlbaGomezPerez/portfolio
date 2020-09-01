b54bf42053ee8652b79ccf9dbd9e68cc2940888b
Alba Gómez Pérez <albagope@gmail.com>
2020-09-01 21:54:12 +0200

    update docs

---
+++ docs/precache-manifest.61d3053b021bd1d46c5a23eeb1cf7d86.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea0e5b6609f1c29b2c9a2c449904596",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cbdb42d09aac31045647",
+    "url": "/portfolio/static/css/2.b08c9e0c.chunk.css"
+  },
+  {
+    "revision": "82d5fc9d3fbdef5bed1d",
+    "url": "/portfolio/static/css/main.dc4633dd.chunk.css"
+  },
+  {
+    "revision": "cbdb42d09aac31045647",
+    "url": "/portfolio/static/js/2.a67ae758.chunk.js"
+  },
+  {
+    "revision": "82d5fc9d3fbdef5bed1d",
+    "url": "/portfolio/static/js/main.df978241.chunk.js"
+  },
+  {
+    "revision": "61a31b95fdfb93fd6aa4",
+    "url": "/portfolio/static/js/runtime-main.dcb3ef65.js"
+  },
+  {
+    "revision": "4c5a83497734523b945b3b5da2d919ad",
+    "url": "/portfolio/static/media/Computer.4c5a8349.mp4"
+  },
+  {
+    "revision": "ed418d7ce71ff9b2f11516b59d9f4d01",
+    "url": "/portfolio/static/media/alba.ed418d7c.jpg"
+  },
+  {
+    "revision": "d28758b48a6069968cbce46af0c1f97f",
+    "url": "/portfolio/static/media/angular.d28758b4.png"
+  },
+  {
+    "revision": "4e29633e338a25fa44b9a9a661c07c18",
+    "url": "/portfolio/static/media/ant.4e29633e.png"
+  },
+  {
+    "revision": "934318536e5c1820423653cbdee0115c",
+    "url": "/portfolio/static/media/css.93431853.png"
+  },
+  {
+    "revision": "16591a880dcc8460b7d8c9b05cb37efb",
+    "url": "/portfolio/static/media/gitHub.16591a88.png"
+  },
+  {
+    "revision": "53d29e80bab77c5d784f9b55c7451458",
+    "url": "/portfolio/static/media/gmail-original.53d29e80.png"
+  },
+  {
+    "revision": "10993a910ad28f1946f2ed4d2e54897f",
+    "url": "/portfolio/static/media/gmail.10993a91.png"
+  },
+  {
+    "revision": "55c319fe549f48252b2d9fdedd277d28",
+    "url": "/portfolio/static/media/html.55c319fe.png"
+  },
+  {
+    "revision": "293bbf0a8f70c417ac29e19e4e6d4313",
+    "url": "/portfolio/static/media/javascript.293bbf0a.png"
+  },
+  {
+    "revision": "1ce042273614d52995649c592c6ef655",
+    "url": "/portfolio/static/media/linkedin-original.1ce04227.png"
+  },
+  {
+    "revision": "073b663f111866558f1ca88cda9a221b",
+    "url": "/portfolio/static/media/linkedin.073b663f.png"
+  },
+  {
+    "revision": "0df3ec686a428a23814a918318942d92",
+    "url": "/portfolio/static/media/materialize.0df3ec68.png"
+  },
+  {
+    "revision": "5a0770ef0c0ffe573f14fbf94ae11e8f",
+    "url": "/portfolio/static/media/react.5a0770ef.png"
+  },
+  {
+    "revision": "05ce6075a78fee79498e4df222c098d2",
+    "url": "/portfolio/static/media/twitter-original.05ce6075.png"
+  },
+  {
+    "revision": "4f19c98b8d17010783878b7f60a6430e",
+    "url": "/portfolio/static/media/twitter.4f19c98b.png"
+  },
+  {
+    "revision": "4e96f1110cb872407e837d4fc1440f96",
+    "url": "/portfolio/static/media/typescript.4e96f111.png"
+  }
+]);
