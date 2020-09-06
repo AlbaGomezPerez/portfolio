06a5cbb13b4d1b79fbdbd90cafd40e155a54350d
Alba Gómez Pérez <albagope@gmail.com>
2020-09-06 17:44:39 +0200

    Fix landing styles

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import {HashLink as Link} from 'react-router-hash-link';
 import '../styles/landing.css';
 import Wave from 'react-wavify';
 import AOS from 'aos';
@@ -13,63 +13,63 @@ const Landing = () => {
 
 
     return (
-        <div className="landing-video" data-aos="welcome-page">
-            <video autoPlay muted loop id="myVideo">
-                <source src={video} type="video/mp4"></source>
-            </video>
+        <React.Fragment>
+            <div className="landing-video" data-aos="welcome-page">
+                <video autoPlay muted loop id="myVideo">
+                    <source src={video} type="video/mp4"></source>
+                </video>
 
-            <div className="landing-text">
-            <h2 className="landing-name">Alba Gómez</h2>
-            <span className="landing-subtext">Desarrolladora Front-End</span>
+                <div className="landing-text">
+                    <h2 className="landing-name">Alba Gómez</h2>
+                    <span className="landing-subtext">Desarrolladora Front-End</span>
 
-            <div className="landing-networks">
-                <div className="landing-logo">
-                    <a href="https://twitter.com/GomezPerezAlba" target="_blank" rel="noopener noreferrer">
-                    <div className="network-image-landing twitterLanding"></div>
-                    </a>
+                    <div className="landing-networks">
+                        <div className="landing-logo">
+                            <a href="https://twitter.com/GomezPerezAlba" target="_blank" rel="noopener noreferrer">
+                                <div className="network-image-landing twitterLanding"></div>
+                            </a>
+                        </div>
+                        <div className="landing-logo">
+                            <a href="https://www.linkedin.com/in/albagomezperez/" target="_blank" rel="noopener noreferrer">
+                                <div className="network-image-landing linkedinLanding"></div>
+                            </a>
+                        </div>
+                        <div className="landing-logo">
+                            <a href="https://github.com/AlbaGomezPerez" target="_blank" rel="noopener noreferrer">
+                                <div className="network-image-landing githubLanding"></div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div className="landing-logo">
-                    <a href="https://www.linkedin.com/in/albagomezperez/" target="_blank" rel="noopener noreferrer">
-                    <div className="network-image-landing linkedinLanding" ></div>
-                    </a>
-                </div>
-                <div className="landing-logo">
-                    <a href="https://github.com/AlbaGomezPerez" target="_blank" rel="noopener noreferrer">
-                    <div className="network-image-landing githubLanding" ></div>
-                    </a>
-                </div>
-            </div>
+                <Wave
+                    mask="url(#mask)"
+                    fill="white"
+                    className="wave"
+                    options={{points: 4, speed: 0.2, amplitude: 20}}>
+                    <defs>
+                        <linearGradient id="gradient" gradientTransform="rotate(90)">
+                            <stop offset="10%" stopColor="white"/>
+                            <stop offset="90%" stopColor="black"/>
+                        </linearGradient>
+                    </defs>
+                </Wave>
+                <Wave
+                    mask="url(#mask)"
+                    fill="white"
+                    className="wave2"
+                    options={{points: 4, speed: 0.2, amplitude: 26}}>
+                    <defs>
+                        <linearGradient id="gradient" gradientTransform="rotate(90)">
+                            <stop offset="10%" stopColor="red"/>
+                            <stop offset="90%" stopColor="blue"/>
+                        </linearGradient>
+                    </defs>
+                </Wave>
             </div>
             <Link className="link-icon" to="#aboutMe">
-             <i className="large material-icons arrow">arrow_drop_down</i>
+                <i className="large material-icons arrow">arrow_drop_down</i>
             </Link>
-
-            <Wave
-                mask="url(#mask)"
-                fill="white"
-                className="wave"
-                options={{ points: 4, speed: 0.2, amplitude: 42 }}>
-                <defs>
-                    <linearGradient id="gradient" gradientTransform="rotate(90)">
-                        <stop offset="10%" stopColor="white" />
-                        <stop offset="90%" stopColor="black" />
-                    </linearGradient>
-                </defs>
-            </Wave>
-            <Wave
-                mask="url(#mask)"
-                fill="white"
-                className="wave2"
-                options={{ points: 4, speed: 0.2, amplitude: 40 }}>
-                <defs>
-                    <linearGradient id="gradient" gradientTransform="rotate(90)">
-                        <stop offset="10%" stopColor="red" />
-                        <stop offset="90%" stopColor="blue" />
-                    </linearGradient>
-                </defs>
-            </Wave>
-
-        </div>
+        </React.Fragment>
     );
 };
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,59 +1,53 @@
 import React from 'react';
 import '../styles/main.css';
 import PropTypes from 'prop-types';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Route, Switch } from 'react-router';
+import {Route, Switch} from 'react-router';
 import ProjectDetail from './projects/ProjectDetail';
-
-
 import AboutMe from './aboutMe/AboutMe';
 import Landing from './Landing';
 
 
-function Main (props) {
-    const { allProjects, allImages, allReadmes } = props;
+function Main(props) {
+    const {allProjects, allImages, allReadmes} = props;
 
-
-  return (
-    <div className="main">
-      <Switch>
-          <Route
-              exact
-              path="/project/:id"
-              render={routerProps => (
-                  <div style={{ overflow: 'hidden' }}>
-                  <ProjectDetail
-                      Match={routerProps.match}
-                      allProjects={allProjects}
-                      allImages={allImages}
-                      allReadmes={allReadmes}
-                  />
-                  </div>
-              )}
-          />
-        <Route
-            path="/"
-            render={routerProps => (
-                <React.Fragment>
-                    <Landing/>
-                        <div style={{ overflow: 'hidden' }}>
-                          <AboutMe
-                              allProjects={allProjects}
-                              allImages={allImages}
-                              allReadmes={allReadmes}
-                          />
+    return (
+        <Switch>
+            <Route
+                exact
+                path="/project/:id"
+                render={routerProps => (
+                    <div style={{overflow: 'hidden'}}>
+                        <ProjectDetail
+                            Match={routerProps.match}
+                            allProjects={allProjects}
+                            allImages={allImages}
+                            allReadmes={allReadmes}
+                        />
+                    </div>
+                )}
+            />
+            <Route
+                path="/"
+                render={routerProps => (
+                    <React.Fragment>
+                        <Landing/>
+                        <div className="main">
+                            <AboutMe
+                                allProjects={allProjects}
+                                allImages={allImages}
+                                allReadmes={allReadmes}
+                            />
                         </div>
-                </React.Fragment>
-            )}
-        />
+                    </React.Fragment>
+                )}
+            />
 
-      </Switch>
-    </div>
-  );
+        </Switch>
+    );
 };
 
 Main.propTypes = {
-  allProjects: PropTypes.array,
+    allProjects: PropTypes.array,
 
 };
 
