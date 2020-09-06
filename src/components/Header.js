6a16cab8d7d03262beadb84b4c178c06278f2074
Alba Gómez Pérez <albagope@gmail.com>
2020-09-06 14:25:31 +0200

    remove nav animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,10 @@ import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import '../styles/header.css';
 // import { Link } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const Header = () => {
-    AOS.init({
-        duration: 100000,
-    });
-
     return (
-          <nav className="nav-wrapper" data-aos="nav-animation">
+          <nav className="nav-wrapper">
             <ul className="nav-info">
                 <Link className="link" to="#aboutMe">
                     <li className="tab-container"><p className="nav-tab">Sobre mi</p></li>
