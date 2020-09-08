f7df7558873af4a34752dcfc69e104dbba52fdee
Alba Gómez Pérez <albagope@gmail.com>
2020-09-08 20:22:04 +0200

    update jest version and add browserRouter

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -5,32 +5,24 @@ import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 
 
-// test("Render footer and home components", () => {
-//     const { container } = render(
-//         <BrowserRouter basename="/portfolio">
-//             <App />
-//         </BrowserRouter>
-//     );
-//     const header = container.querySelector("header");
-//     const nav = container.querySelector(".nav-wrapper");
-//
-//     expect(header).not.toBe(null);
-//     expect(nav).not.toBe(null);
-//
-//     const footer = container.querySelector("#footer");
-//
-//     expect(footer.textContent).toBe(
-//         "Alba Gómez Pérez ©2020"
-//     );
-//
-//     const home = container.querySelector(".main");
-//
-//     expect(home).not.toBe(null);
-// });
+test("Render footer and home components", () => {
+    const { container } = render(
+        <BrowserRouter basename="/portfolio">
+            <App />
+        </BrowserRouter>
+    );
+    const header = container.querySelector(".app");
+
+    expect(header).not.toBe(null);
+
+});
 
 
 test("Filters component render input text", () => {
-    const { container } = render(<Landing />);
+    const { container } = render(
+        <BrowserRouter>
+        <Landing />
+        </BrowserRouter>);
     const video = container.querySelector("#myVideo");
 
     expect(video).not.toBe(null);
