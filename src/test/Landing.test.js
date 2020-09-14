473af0402950068f8e1b0074738eecfed35f1291
Alba Gómez Pérez <albagope@gmail.com>
2020-09-14 11:43:39 +0200

    Create tests to landing component

--- src/test/Landing.test.js
+++ src/test/Landing.test.js
@@ -4,14 +4,33 @@ import Landing from "../components/Landing";
 import { BrowserRouter } from "react-router-dom";
 
 
-
-
 test("Landing component render video", () => {
     const { container } = render(
         <BrowserRouter>
             <Landing />
         </BrowserRouter>);
-    const video = container.querySelector("#myVideo");
 
+    //Video
+    const video = container.querySelector("#myVideo");
     expect(video).not.toBe(null);
+
+    //Networks logos with their links
+    const twitter = container.querySelector(".twitterLanding");
+    expect(twitter).not.toBe(null);
+    expect(twitter.parentElement.href).toBe("https://twitter.com/GomezPerezAlba");
+
+    const linkedin = container.querySelector(".linkedinLanding");
+    expect(linkedin).not.toBe(null);
+    expect(linkedin.parentElement.href).toBe("https://www.linkedin.com/in/albagomezperez/");
+
+    const github = container.querySelector(".githubLanding");
+    expect(github).not.toBe(null);
+    expect(github.parentElement.href).toBe("https://github.com/AlbaGomezPerez");
+
+    //Waves
+    const wave = container.querySelector(".wave");
+    expect(wave).not.toBe(null);
+    const wave2 = container.querySelector(".wave2");
+    expect(wave2).not.toBe(null);
 });
+
