4de27336fe4ff214c66ff2f2bb205929fcae74e1
Alba Gómez Pérez <albagope@gmail.com>
2020-09-15 12:52:45 +0200

    add testing to nav links

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -10,15 +10,20 @@ test("Render footer, header and main components", () => {
             <App />
         </BrowserRouter>
     );
+
+    //Header
     const headerNav = container.querySelector(".nav-wrapper");
     expect(headerNav).not.toBe(null);
+    const navLinks = container.querySelectorAll(".link");
+    expect(navLinks[0].href).toBe("http://localhost/#aboutMe");
+    expect(navLinks[1].href).toBe("http://localhost/#projects");
+    expect(navLinks[2].href).toBe("http://localhost/#contact");
 
+    //Main
     const main = container.querySelector(".main");
     expect(main).not.toBe(null);
 
-
+    //Footer
     const footerText = container.querySelector("#footer");
-    expect(footerText.textContent).toBe(
-        "Alba Gómez Pérez ©2020"
-    );
+    expect(footerText.textContent).toBe("Alba Gómez Pérez ©2020");
 });
